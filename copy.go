77c0878417f8c13fb097383d9faf6518ebf439ef
Achille Roussel <achille@segment.com>
2016-12-31 14:04:48 -0800

    [skip ci] add comment

--- copy.go
+++ copy.go
@@ -8,14 +8,17 @@ import (
 // Copy behaves exactly like io.Copy but uses an internal buffer pool to release
 // pressure off of the garbage collector.
 func Copy(w io.Writer, r io.Reader) (n int64, err error) {
+	// Check for io.WriterTo and io.ReaderFrom so we don't hold a buffer during
+	// the copy if one of these interfaces is already implemented, io.CopyBuffer
+	// will double-check on that and fail but that's OK, the cost is likely
+	// going to be small compared to the rest of the time spent moving bytes
+	// from the reader to the writer.
 	if from, ok := r.(io.WriterTo); ok {
 		return from.WriteTo(w)
 	}
-
 	if to, ok := w.(io.ReaderFrom); ok {
 		return to.ReadFrom(r)
 	}
-
 	buf := bufferPool.Get().(*buffer)
 	n, err = io.CopyBuffer(w, r, buf.b)
 	bufferPool.Put(buf)
