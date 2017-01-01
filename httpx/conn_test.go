4e58f378973c36a953bf9512573274a61235c3ea
Achille Roussel <achille@segment.com>
2016-12-31 18:52:55 -0800

    add connReader test

--- httpx/conn_test.go
+++ httpx/conn_test.go
@@ -2,6 +2,8 @@ package httpx
 
 import (
 	"bufio"
+	"io"
+	"net"
 	"net/http"
 	"testing"
 	"time"
@@ -26,4 +28,36 @@ func TestConnTransportConfigured(t *testing.T) {
 			MaxResponseHeaderBytes: 65536,
 		}
 	})
+}
+
+func TestConnReader(t *testing.T) {
+	c1, c2 := net.Pipe()
+	defer c1.Close()
+	defer c2.Close()
+
+	ok := false
+	cr := &connReader{
+		Conn:   c1,
+		limit:  10,
+		cancel: func() { ok = true },
+	}
+
+	go c2.Write([]byte("Hello World!"))
+	var b [16]byte
+
+	n, err := cr.Read(b[:])
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if n > 10 {
+		t.Error("too many bytes read from c1:", n)
+		return
+	}
+
+	if _, err := cr.Read(b[:]); err != io.EOF {
+		t.Error("expected io.EOF but got", err)
+		return
+	}
 }
